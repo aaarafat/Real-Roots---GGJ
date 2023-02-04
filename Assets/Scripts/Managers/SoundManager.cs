9ac4a7c12ace93395462d0b51cf282b9a7c67cdb
Darkin <aaarafat@yahoo.com>
2023-02-05 00:10:24 +0200

    Don't Destroy SoundManager

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -13,7 +13,7 @@ public class SoundManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            //DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(gameObject);
         }
         else Destroy(gameObject);
 
