3814d02410e40f71d030ea3e2e74bfd896118bf6
Darkin <aaarafat@yahoo.com>
2023-02-04 23:05:06 +0200

    Reload on Death

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,8 +8,7 @@ public class GameManager : MonoBehaviour
 
 
     public static GameManager Instance;
-    public static event Action<float> OnSpeedChange;
-    public float _speed;
+    public static event Action OnPlayerDeath;
     private void Awake()
     {
         if (Instance == null)
@@ -23,16 +22,22 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        OnSpeedChange?.Invoke(_speed);
+
     }
 
     private void OnValidate()
     {
-        OnSpeedChange?.Invoke(_speed);
+
     }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Die()
+    {
+        OnPlayerDeath?.Invoke();
+        Loader.ReloadCurrentScene();
     }
 }
