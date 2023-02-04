a90948553f6aff6ebffd80416ce23b7228e44860
Darkin <aaarafat@yahoo.com>
2023-02-04 18:50:44 +0200

    Levels Structure

---
+++ Assets/Scripts/Utils/Loader.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+
+    private class LoadingMonoBehaviour : MonoBehaviour { }
+
+    public enum Scene
+    {
+        Loading,
+    }
+
+    private static Action onLoaderCallback;
+    private static AsyncOperation loadingAsyncOperation;
+
+    public static void Load(string scene)
+    {
+        // Set the loader callback action to load the target scene
+        onLoaderCallback = () => {
+            GameObject loadingGameObject = new GameObject("Loading Game Object");
+            loadingGameObject.AddComponent<LoadingMonoBehaviour>().StartCoroutine(LoadSceneAsync(scene));
+        };
+
+        // Load the loading scene
+        SceneManager.LoadScene("Loading");
+    }
+
+    private static IEnumerator LoadSceneAsync(string scene)
+    {
+        yield return null;
+
+        loadingAsyncOperation = SceneManager.LoadSceneAsync(scene);
+
+        while (!loadingAsyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public static float GetLoadingProgress()
+    {
+        if (loadingAsyncOperation != null)
+        {
+            return loadingAsyncOperation.progress;
+        }
+        else
+        {
+            return 1f;
+        }
+    }
+
+    public static void LoaderCallback()
+    {
+        // Triggered after the first Update which lets the screen refresh
+        // Execute the loader callback action which will load the target scene
+        if (onLoaderCallback != null)
+        {
+            onLoaderCallback();
+            onLoaderCallback = null;
+        }
+    }
+}
